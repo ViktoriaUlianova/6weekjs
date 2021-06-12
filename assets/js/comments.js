2ee038a26cef41f899fbec22ff3e0a796529f356
Viktoria <viktoria.ulianova@mail.ru>
2021-06-12 17:59:50 +0700

    added storage name

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,9 +1,33 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let storageName = localStorage.getItem('name');
+
+    if (storageName != null) {
+        document.getElementById('one').value = storageName;
+    }
+
+
+    let storagePic = localStorage.getItem('picture');
+
+    if (localStorage.getItem('picture') != null) {
+        document.getElementById("picture").value = storagePic;
+    }
+});
+
 function showComment() {
-    let name = document.getElementById("one").value;
+    let photo = document.getElementById("picture").value;
+    let nickName = document.getElementById("one").value;
     let comment = document.getElementById("two").value;
     comment = checkSpam(comment);
-    name = checkSpam(name);
-    document.getElementById("three").value = name + " : " + comment;
+    nickName = checkSpam(nickName);
+    if (localStorage.getItem('name') == null) {
+        localStorage.setItem('name', nickName);
+    }
+
+    if (localStorage.getItem('picture') == null) {
+        localStorage.setItem('picture', filePicture);
+    }
+
+    document.getElementById("three").value = photo + ":" + nickName + " : " + comment;
 }
 
 function checkSpam(str) {
@@ -11,4 +35,12 @@ function checkSpam(str) {
     str = str.replace(/xxx/ig, "***");
 
     return str;
+}
+
+function showPhoto() {
+
+
+    let filePicture = document.getElementById("picture").value;
+
+
 }
